4d8905bf378d81e258b1edcb6f4de84a5ce8ba2e
Ravi1983-dev <ravishankar.sg2@gmail.com>
2025-05-07 20:39:24 +0530

    Update index.sql

--- index.sql
+++ index.sql
@@ -1,1 +1,21 @@
-insert into dbo.[empinfo] values (2,'Shankar')
+-- Rebuild all indexes on all user tables
+DECLARE @TableName NVARCHAR(255)
+
+DECLARE TableCursor CURSOR FOR
+SELECT QUOTENAME(SCHEMA_NAME(schema_id)) + '.' + QUOTENAME(name)
+FROM sys.tables
+WHERE is_ms_shipped = 0 -- exclude system tables
+
+OPEN TableCursor
+FETCH NEXT FROM TableCursor INTO @TableName
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    PRINT 'Rebuilding indexes on ' + @TableName
+    EXEC('ALTER INDEX ALL ON ' + @TableName + ' REBUILD')
+    PRINT('ALTER INDEX ALL ON ' + @TableName + ' REBUILD')
+    FETCH NEXT FROM TableCursor INTO @TableName
+END
+
+CLOSE TableCursor
+DEALLOCATE TableCursor
